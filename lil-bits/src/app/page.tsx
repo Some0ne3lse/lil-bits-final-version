fdc18848bd4b8b6d9392d783328e9d1973cd815b
Some0ne3lse <143652507+Some0ne3lse@users.noreply.github.com>
2024-06-02 22:57:04 +0000

    Homepage next size done

--- lil-bits/src/app/page.tsx
+++ lil-bits/src/app/page.tsx
@@ -9,17 +9,21 @@ import LinkButton from "./global-components/LinkButton";
 const Home = () => {
   return (
     <main>
-      <Header />
-      <TheCarousel />
-      <div className={styles.order_container}>
-        <div className={styles.order_box}>
-          <p className={styles.order_text}>Go to our order screen!</p>
-          <LinkButton link="/select-dish" text="Order" />
+      <div className={styles.entire_home_page}>
+        <Header />
+        <TheCarousel />
+        <div className={styles.homepage_buttons}>
+          <div className={styles.order_container}>
+            <div className={styles.order_box}>
+              <p className={styles.order_text}>Go to our order screen!</p>
+              <LinkButton link="/select-dish" text="Order" />
+            </div>
+          </div>
+          <SearchForEmail />
         </div>
+        <Address />
+        <Footer />
       </div>
-      <SearchForEmail />
-      <Address />
-      <Footer />
     </main>
   );
 };
