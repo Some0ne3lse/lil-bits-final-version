de3300f3c143c0e905ca00e2beaf8c6e3a7dca61
Some0ne3lse <143652507+Some0ne3lse@users.noreply.github.com>
2024-06-06 21:49:53 +0000

    Minor css changes

--- lil-bits/src/app/(routes)/select-dish/components/MealDescription.tsx
+++ lil-bits/src/app/(routes)/select-dish/components/MealDescription.tsx
@@ -12,7 +12,7 @@ const MealDescription = ({ title, description }: DishDescription) => {
     <>
       <div className={styles.dish_description_box}>
         <div className={styles.dish_description_scroll}>
-          <h1 className={styles.dish_description_text}>{title}</h1>
+          <h1 className={styles.dish_description_title}>{title}</h1>
           <p className={styles.dish_description_text}>{description}</p>
         </div>
       </div>
