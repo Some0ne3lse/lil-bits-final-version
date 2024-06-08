698b7c80525e2686e8c2eb841029f4027705871b
Some0ne3lse <143652507+Some0ne3lse@users.noreply.github.com>
2024-06-08 18:14:16 +0000

    Css changes

--- lil-bits/src/app/(routes)/select-dish/page.tsx
+++ lil-bits/src/app/(routes)/select-dish/page.tsx
@@ -1,6 +1,9 @@
 import AllMeals from "./components/AllMeals";
 
 const SelectDish = () => {
+  // I put all code in AllMeals when I was creating the loading screen.
+  // At the time it was better, since I had header and footer here instead of in layout.tsx
+  // Might change this back later
   return (
     <main>
       <AllMeals />
