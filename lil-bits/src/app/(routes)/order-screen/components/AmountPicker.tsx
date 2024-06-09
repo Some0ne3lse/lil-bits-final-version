09a324ab1a2f94cbcb88d27d6ae503e28f38946f
Some0ne3lse <143652507+Some0ne3lse@users.noreply.github.com>
2024-06-09 21:37:01 +0000

    Comments + Tried to fix label but couldn't

--- lil-bits/src/app/(routes)/order-screen/components/AmountPicker.tsx
+++ lil-bits/src/app/(routes)/order-screen/components/AmountPicker.tsx
@@ -18,30 +18,32 @@ const AmountPicker = ({
   count,
 }: AmountPickerType) => {
   // We pass the above things to props, so it works with the form
-  // In the browser you will get the issue 'No label associated with this field'
-  // I am not sure how to fix this, tried a few different methods
+  // In the app you will get the issue 'No label associated with this field'
+  // I am not sure how to fix this. Methods I tried:
+  // Wrapping label around amount_selector - nothing changed
+  // Giving amount_selector an id and ad htmlFor in label - got a warning that I was using it wrongly
+
   return (
     <div className={styles.amount_container}>
       <label className={styles.form_label_amount}>
         Select amount of people
-        <div className={styles.amount_selector}>
-          <FontAwesomeIcon
-            icon={faAngleDown}
-            onClick={decreaseAmount}
-            style={{ cursor: "pointer" }}
-            className={styles.amount_icon}
-          />
-          <div className={styles.amount_number}>{count.toString()}</div>
-          <FontAwesomeIcon
-            icon={faAngleUp}
-            onClick={increaseAmount}
-            style={{ cursor: "pointer" }}
-            className={styles.amount_icon}
-          />
-        </div>
       </label>
-
-      {/* If the amount is above 10 or below 1 we set the invalidAmount and show it */}
+      <div className={styles.amount_selector}>
+        <FontAwesomeIcon
+          icon={faAngleDown}
+          onClick={decreaseAmount}
+          style={{ cursor: "pointer" }}
+          className={styles.amount_icon}
+        />
+        <div className={styles.amount_number}>{count.toString()}</div>
+        <FontAwesomeIcon
+          icon={faAngleUp}
+          onClick={increaseAmount}
+          style={{ cursor: "pointer" }}
+          className={styles.amount_icon}
+        />
+      </div>
+      {/* If the user tries to select above 10 or below 1 we set the invalidAmount and show it */}
       {invalidAmount && (
         <div className={styles.amount_error}>{invalidAmount}</div>
       )}
